cf58017ea752f5a7ccabc26c960a125dde6a938f
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2023-12-15 09:53:35 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,16 @@
+package ifelse;
+
+public class even {
+	public static void main(String[] args) {
+		int num = 4;
+		if (num % 2 ==0) {
+			System.out.println("number is even");
+		}
+		else {
+			System.out.println("type other number");
+		}
+	}
+
+}
+
+
