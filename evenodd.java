2fa11a5c1eddd601378bc5bdf87a7d6446461fc9
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2023-12-18 09:09:05 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,13 @@
+package ifelse;
+
+public class evenodd {
+	public static void main(String[] args) {
+		int num = 7;
+		if (num % 2 ==0) {
+			System.out.println("number is even");
+		}
+		else {
+			System.out.println("number is odd");
+		}
+	}
+}
