e3ffd1f198af5c9e2e98ee256fa190cfe6cb5226
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2023-12-21 09:13:49 +0530

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,16 @@
+package ifelse;
+
+public class odd {
+	public static void main(String[] args) {
+		int num = 2;
+		if (num % 2 ==0) {
+			System.out.println("type other number");
+		}
+		else {
+			System.out.println("number is odd");
+		}
+	}
+
+}
+
+
