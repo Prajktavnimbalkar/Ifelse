281c3ab140cfb89c080d505386e3b23d0fe2cfdb
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-01-02 09:28:59 +0530

    Add files via upload

---
+++ posneg.java
@@ -0,0 +1,17 @@
+package ifelse;
+
+public class posneg {
+	public static void main(String[] args) {
+		int num =0;
+		if (num <0) {
+			System.out.println("number is negative");
+		}
+		else if(num==0) {
+			System.out.println("zero");
+		}
+		else {
+			System.out.println("numer is positive");
+	}
+
+}
+}
